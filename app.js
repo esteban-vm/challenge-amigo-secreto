a253d39e57dca6fc6cac080e0425231352d83c06
Esteban V.M <estebanvm90@outlook.com>
2025-01-28 16:34:40 -0300

    implementa función para agregar amigos

--- app.js
+++ app.js
@@ -4,3 +4,17 @@
 
 /** @type {string[]} */
 const amigos = [];
+
+function agregarAmigo() {
+  const campoTexto = /** @type {HTMLInputElement} */ (document.getElementById('amigo'));
+  const nombreAmigo = campoTexto.value.trim();
+
+  if (nombreAmigo) {
+    amigos.push(nombreAmigo);
+    campoTexto.value = '';
+  } else {
+    alert('Por favor, inserte un nombre');
+  }
+
+  campoTexto.focus();
+}
